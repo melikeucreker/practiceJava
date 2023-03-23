30ef1386f12108e5c8a233eebad40b236546a97f
melikeucreker <melikeucareker@gmail.com>
2023-03-23 16:09:46 +0300

    practice

---
+++ src/ornk/Foreach.java
@@ -0,0 +1,26 @@
+package ornk;
+
+import java.util.ArrayList;
+
+public class Foreach {
+
+    public static void main(String[] args) {
+        //Array'in icerdigi kelimeleri inceleyin.uzunlugu cift olanların ilk yarısını,
+        //tek olanların son yarısını yazdırın
+
+        String[] stringArr = {"Java", "kendine", "iyi","bak"};
+
+        ArrayList<String> newList = new ArrayList<>();
+        for (String each : stringArr) {
+            int length = each.length();
+            if (length % 2 == 0) {
+                newList.add(each.substring(0, length/2));
+            } else {
+                newList.add(each.substring(length/2));
+            }
+        }
+
+        System.out.println(newList);
+    }
+}
+
